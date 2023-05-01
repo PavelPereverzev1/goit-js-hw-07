fc57a875b5bd6ca7fb18b4f8caa923ab3135a649
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-05-01 22:55:34 +0300

    finished first part

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// створюємо масив елементів списку
+const addedElements = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </li>`;
+});
+
+// додавання елементів до розмітки
+const galleryList = document.querySelector('.gallery');
+galleryList.insertAdjacentHTML('afterbegin', addedElements.join(''));
+
+// додаємо обробник події до галереї
+
+galleryList.addEventListener('click', selectImg);
+
+function selectImg(event) {
+  // відміняємо перехід за посиланням
+  event.preventDefault();
+  //викликаємо створення модального вікна
+  viewOriginal(event.target.dataset.source);
+}
+
+function viewOriginal(url) {
+  const instance = basicLightbox.create(`<img width="1280" src="${url}">`);
+  instance.show();
+  // додаємо закриття по Escape
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
